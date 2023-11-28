bfebd983e666805b42fc8ac1dc21488c3eca9ede
Jobi S S <jobisjobi1234@gmail.com>
2023-11-28 21:15:13 +0530

    still working in pomodoro functionality

--- main_page/pomodoro.py
+++ main_page/pomodoro.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from datetime import timedelta
+from PIL import ImageTk, Image
 import os
 
 
@@ -10,34 +11,55 @@ def pomodoro_func(frame, content: str = 'Whatever content',
 
 class PomodoroTimerApp:
     def __init__(self, master):
-        self.master = master
-        # master.title("Pomodoro Timer")
+        self.master: tk.Frame = master
+        self.master.configure(bg='white',width=750, height=800)
 
-        # print(os.path.abspath('img/tomato.jpg'))
-        # Load an image (replace 'path/to/your/image.png' with the actual path)
-        print("image loaded")
+        temp_path = 'G:/Jobi Development/My Own Projects/Automation Daily Task/img/tomato.jpg'
+        image = Image.open(temp_path)
+        self.image = ImageTk.PhotoImage(image)
 
-        self.image = tk.PhotoImage(name='./../img/tomato.jpg')
-        print("image loaded")
         # Create a Canvas
-        self.canvas = tk.Canvas(master, width=200, height=300)
+        self.canvas = tk.Canvas(master, width=612, height=612)
         self.canvas.pack()
 
         # Display the image on the Canvas
-        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image)
+        self.canvas.create_image(50, 50, anchor=tk.NW, image=self.image)
+
+        title_text = "Pomodoro"
+        self.canvas.create_text(350, 350, text=title_text, font=("Helvetica", 24, 'bold'), fill="white")
 
         # Initialize timer duration
         self.timer_duration = timedelta(minutes=25)
 
         # Create a label to display the timer
-        self.timer_label = tk.Label(master, text=self.format_time(self.timer_duration), font=("Helvetica", 24))
-        self.timer_label.pack()
+        self.timer_label = tk.Label(master, text=self.format_time(self.timer_duration), font=("Helvetica", 24),
+                                    pady=10, padx=5, bg="#CD0508", fg="black")
+        self.timer_label.config(highlightthickness=0, highlightbackground="white")
+        # self.timer_label.pack()
+        self.timer_label_window = self.canvas.create_window(350, 400,anchor=tk.N, window=self.timer_label)
 
         # Create Start and Reset buttons
-        self.start_button = tk.Button(master, text="Start", command=self.start_timer)
-        self.start_button.pack(side=tk.LEFT, padx=10)
-        self.reset_button = tk.Button(master, text="Reset", command=self.reset_timer)
-        self.reset_button.pack(side=tk.RIGHT, padx=10)
+        # self.start_button = tk.Button(master, text="Start", command=self.start_timer, padx=10, pady=5,
+        #                               bg='light green',font=("Helvetica", 18))
+        # self.start_button.place(x=50, y=700)
+        # self.reset_button = tk.Button(master, text="Reset", command=self.reset_timer, padx=10, pady=5, bg='sky blue',
+        #                               font=("Helvetica", 18))
+        # self.reset_button.place(x=515, y=500)
+
+        # self.start_button = tk.Button(self.canvas, text="Start", command=self.start_timer, padx=10, pady=5,
+        #                               bg='light green', font=("Helvetica", 18))
+        # self.canvas.create_window(50, 700, anchor=tk.NW, window=self.start_button)
+        #
+        # self.reset_button = tk.Button(self.canvas, text="Reset", command=self.reset_timer, padx=10, pady=5,
+        #                               bg='sky blue', font=("Helvetica", 18))
+        # self.canvas.create_window(515, 500, anchor=tk.NW, window=self.reset_button)
+
+        self.start_button = tk.Button(master, text="Start", command=self.start_timer, padx=10, pady=5,
+                                      bg='light green', font=("Helvetica", 18))
+        self.start_button.pack()
+        self.reset_button = tk.Button(master, text="Reset", command=self.reset_timer, padx=10, pady=5, bg='sky blue',
+                                      font=("Helvetica", 18))
+        self.reset_button.pack()
 
         # Initialize timer state
         self.timer_running = False
